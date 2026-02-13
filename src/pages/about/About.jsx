eee1b60ee593c2ce4a859b09669ec2eee68fd7c4
VohobjonovSadorbek <vohobjonovsardorbek2005@gmail.com>
2026-02-14 00:48:36 +0500

    some changed

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <h1>
+      salom
+    </h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quidem molestias, ipsam ex cum magni nisi, vitae harum modi velit autem. Officiis qui quam labore voluptates libero aut sit quidem similique earum beatae maxime amet quod ratione iusto neque expedita explicabo nesciunt, quibusdam cumque alias laboriosam veritatis illo! Deleniti alias delectus officia necessitatibus, accusantium aut magnam, libero corporis fugit corrupti, debitis nisi odit impedit a amet? Vero fugit quos laboriosam iste fugiat nostrum ullam eveniet dignissimos ipsa placeat. Amet aperiam, at dolor architecto saepe, esse vel tempora, exercitationem maiores impedit nostrum dolores recusandae voluptates! Incidunt similique est voluptatibus impedit hic?
+    </p>
+  </div>;
 }
 
 export default About;
